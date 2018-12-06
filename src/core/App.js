a965515934c8ae4378fc56db955c99117a1b3121
Anna Branco <annya.branco@gmail.com>
2018-12-06 15:05:27 +0100

    add: light effects

--- src/core/App.js
+++ src/core/App.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react';
 import Background from '../components/background-component/background-component';
 
 class App extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			light : 'day',
+			cloud : '',
+		};
+	}
+
+	propsValues = {
+		light : [ 'neutral', 'day', 'twilight', 'night' ],
+		cloud : [ '', 'clouds', 'moreclouds', 'itrains' ],
+	};
+
 	render() {
+		const {
+			light,
+			cloud,
+		} = this.state;
+
 		return (
 			<div className="App">
-				<Background />
+				<Background
+					light = { light }
+					cloud = { cloud }
+					propsValues = { this.propsValues }
+				/>
 			</div>
 		);
 	}
